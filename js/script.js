0421771f467e703cf378836c9f88d6d1be2f97d3
NastiaMikhaltsova <mihaltsovan@gmail.com>
2018-06-23 00:03:59 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.onload= function(){
+	var width = window.innerWidth;
+	var height=window.innerHeight;
+	var canvas = document.getElementById('canvas');
+	
+	canvas.setAttribute('width', width);
+	canvas.setAttribute('height',height);
+	
+	var renderer = new THREE.WebGLRenderer({canvas: canvas});
+	renderer.setClearColor(0x000000);
+	
+	var scene = new THREE.Scene();
+	
+	var camera= new. THREE.PerspectiveCamera(45, width  / height, 0.1, 5000);
+	camera.position.set(0, 0, 1000);
+	
+	renderer.render(scene, camera);
+	
+}
